af633c57bba49b6fc325f347f51cac551072d96c
Andres Miguel Rubio (External) <andresmiguel.rubio@odigeo.com>
2017-02-06 10:13:47 +0100

    First commit

---
+++ app/src/main/java/upcomingmovies/andresmr/com/upcomingmovies/MainActivity.java
@@ -0,0 +1,13 @@
+package upcomingmovies.andresmr.com.upcomingmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
