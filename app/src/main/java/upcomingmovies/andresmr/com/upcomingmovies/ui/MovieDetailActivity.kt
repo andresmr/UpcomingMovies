41346d7046e4f9b838951121126c061ceca9da51
Andres Miguel Rubio (External) <andresmiguel.rubio@edreamsodigeo.com>
2017-03-13 15:48:08 +0100

    Import Anko Layout

--- app/src/main/java/upcomingmovies/andresmr/com/upcomingmovies/ui/MovieDetailActivity.kt
+++ app/src/main/java/upcomingmovies/andresmr/com/upcomingmovies/ui/MovieDetailActivity.kt
@@ -1,9 +1,9 @@
 package upcomingmovies.andresmr.com.upcomingmovies.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_movie_detail.*
-import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.*
 import upcomingmovies.andresmr.com.upcomingmovies.R
 import upcomingmovies.andresmr.com.upcomingmovies.data.entities.Result
 
@@ -12,7 +12,19 @@ class MovieDetailActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
-        val movie : Result? = intent.getSerializableExtra("movie") as? Result
-        movie_title.text = movie?.title
+        val result : Result? = intent.getSerializableExtra("movie") as? Result
+        if(result != null) {
+
+            verticalLayout {
+                padding = dip(10)
+                val name = textView {
+                    textColor = Color.BLACK
+                    textSize = 20f
+                }
+                val overview = textView()
+                name.text = result.title
+                overview.text = result.overview
+            }
+        }
     }
 }
